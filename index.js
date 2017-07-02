32297b24e3e5a7f6123cf7f7be242a597143f4cc
Martin Hansen <martin@martinhansen.no>
2017-07-02 14:56:01 +0200

    Added index file

--- index.js
+++ index.js
@@ -1,10 +1,52 @@
 'use strict';
-module.exports = (input, opts) => {
-	if (typeof input !== 'string') {
-		throw new TypeError(`Expected a string, got ${typeof input}`);
-	}
+module.exports = AsyncMethods = {
+  install(Vue, options) {
+    options = options || {}
 
-	opts = opts || {};
+    function wrapMethod(func, vm) {
+      function wrapped() {
+        var args = [].slice.call(arguments)
 
-	return input + ' & ' + (opts.postfix || 'rainbows');
-};
+        wrapped.isCalled = true
+        wrapped.isPending = true
+        wrapped.isResolved = false
+        wrapped.isRejected = false
+
+        var result = func.apply(vm, args)
+
+        if (result && result.then) {
+          result.then(function(res) {
+            wrapped.isPending = false
+            wrapped.isResolved = true
+
+            return res
+          }).catch(function(err) {
+            wrapped.isPending = false
+            wrapped.isRejected = true
+
+            throw err
+          })
+
+          return result
+        } else {
+          return result
+        }
+      }
+
+      wrapped.isCalled = false
+      wrapped.isPending = false
+      wrapped.isResolved = false
+      wrapped.isRejected = false
+
+      return wrapped
+    }
+
+    Vue.mixin({
+      beforeCreate() {
+        for (const key in this.$options.asyncMethods || {}) {
+          this[key] = wrapMethod(this.$options.asyncMethods[key], this)
+        }
+      }
+    })
+  }
+}
