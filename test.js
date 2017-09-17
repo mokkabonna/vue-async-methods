4a4dc975ca6a5c0973f13331f5327c332d9d178d
Martin Hansen <martin@martinhansen.no>
2017-09-17 20:41:51 +0000

    Test for called

--- test.js
+++ test.js
@@ -24,17 +24,35 @@ describe('vue-async-methods', function() {
   })
   
   it('creates the method object on the vm', function() {
-	expect(vm.fetch.execute).to.be.a('function')
+  	expect(vm.fetch.execute).to.be.a('function')
   })
   
   it('exposes the initial state', function() {
-	expect(vm.fetch.isCalled).to.equal(false)
-	expect(vm.fetch.isPending).to.equal(false)
-	expect(vm.fetch.isResolved).to.equal(false)
-	expect(vm.fetch.isRejected).to.equal(false)
-	expect(vm.fetch.resolvedWith).to.equal(null)
-	expect(vm.fetch.resolvedWithSomething).to.equal(false)
-	expect(vm.fetch.resolvedWithEmpty).to.equal(false)
-	expect(vm.fetch.rejectedWith).to.equal(null)
+  	expect(vm.fetch.isCalled).to.equal(false)
+  	expect(vm.fetch.isPending).to.equal(false)
+  	expect(vm.fetch.isResolved).to.equal(false)
+  	expect(vm.fetch.isRejected).to.equal(false)
+  	expect(vm.fetch.resolvedWith).to.equal(null)
+  	expect(vm.fetch.resolvedWithSomething).to.equal(false)
+  	expect(vm.fetch.resolvedWithEmpty).to.equal(false)
+  	expect(vm.fetch.rejectedWith).to.equal(null)
+  })
+  
+  describe('after called', function() {
+  	var call
+  	beforeEach(function() {
+  	  call = vm.fetch.execute()
+  	})
+  	
+  	it('is called', function() {
+  	  expect(vm.fetch.isCalled).to.equal(true)
+    	expect(vm.fetch.isPending).to.equal(true)
+    	expect(vm.fetch.isResolved).to.equal(false)
+    	expect(vm.fetch.isRejected).to.equal(false)
+    	expect(vm.fetch.resolvedWith).to.equal(null)
+    	expect(vm.fetch.resolvedWithSomething).to.equal(false)
+    	expect(vm.fetch.resolvedWithEmpty).to.equal(false)
+    	expect(vm.fetch.rejectedWith).to.equal(null)
+  	})
   })
 })
