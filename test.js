e4def7930ba27c6e94a10eba2e609c0518184aeb
Martin Hansen <martin@martinhansen.no>
2017-09-17 20:34:44 +0000

    First tests

--- test.js
+++ test.js
@@ -1,11 +1,40 @@
-import test from 'ava'
-import m from '.'
+/* global describe, it */
+var expect = require('chai').expect
+var Vue = require('vue')
+var asyncMethods = require('./index')
+var resolve
+var reject
 
-test('title', t => {
-  const err = t.throws(() => {
-    m(123)
-  }, TypeError)
-  t.is(err.message, 'Expected a string, got number')
+function fetch() {
+  return new Promise(function(res, rej) {
+    resolve = res
+    reject = rej
+  })
+}
 
-  t.is(m('unicorns'), 'unicorns & rainbows')
+describe('vue-async-methods', function() {
+  var vm
+  beforeEach(function() {
+  	Vue.use(asyncMethods)
+  	vm = new Vue({
+  	  asyncMethods: {
+  	  	fetch: fetch
+  	  }
+  	})
+  })
+  
+  it('creates the method object on the vm', function() {
+	expect(vm.fetch.execute).to.be.a('function')
+  })
+  
+  it('exposes the initial state', function() {
+	expect(vm.fetch.isCalled).to.equal(false)
+	expect(vm.fetch.isPending).to.equal(false)
+	expect(vm.fetch.isResolved).to.equal(false)
+	expect(vm.fetch.isRejected).to.equal(false)
+	expect(vm.fetch.resolvedWith).to.equal(null)
+	expect(vm.fetch.resolvedWithSomething).to.equal(false)
+	expect(vm.fetch.resolvedWithEmpty).to.equal(false)
+	expect(vm.fetch.rejectedWith).to.equal(null)
+  })
 })
